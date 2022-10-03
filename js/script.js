f5238141f6f9f220be91161a514bb6d34e381d60
Shwartz <examlpe@gmail.com>
2022-10-03 21:06:46 +0300

    callback

--- js/script.js
+++ js/script.js
@@ -1,31 +1,27 @@
-// 'use strict';
+'use strict';
 
-// function first() {
-//   // что-то присходит
-//   setTimeout(function() {
-//     console.log(1);
-//   }, 500);
-// }
-
-// function second() {
-//   console.log(2);
-// }
-
-// first();
-// second();
-
-// function learnJS (lang, callback) {
-//   console.log(`Я учу: ${lang}`);
-//   callback();
-// }
-
-// function done() {
-//   console.log('Я прошёл этот урок');
-// }
-
-// learnJS('JavaScript', done);
-
-function myFunc() {
-  const a = [4, 5, 6];
-  document.querySelector('out-1').innerHTML = a.join('-');
+function first() {
+  // что-то присходит
+  setTimeout(function() {
+    console.log(1);
+  }, 500);
 }
+
+function second() {
+  console.log(2);
+}
+
+first();
+second();
+
+function learnJS (lang, callback) {
+  console.log(`Я учу: ${lang}`);
+  callback();
+}
+
+function done() {
+  console.log('Я прошёл этот урок');
+}
+
+learnJS('JavaScript', done);
+
