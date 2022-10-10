7aa747d0155fcb709e298e856dece5600438d577
Shwartz <examlpe@gmail.com>
2022-10-10 13:19:20 +0300

    передача по ссылке и значению

--- js/script.js
+++ js/script.js
@@ -1,33 +1,95 @@
-const options = {
-  name: 'test',
-  width: 1024,
-  height: 1024,
-  colors: {
-    border: 'black',
-    bg: 'red'
-  },
-  makeTest: function() {
-    console.log('test');
+/*jshint esnext: true */
+
+let a = 5,
+    b = a;
+
+b = b + 5;
+
+console.log(b);
+console.log(a);
+
+const obj = {
+  a: 5,
+  b: 1
+};
+
+const copy = obj; // Ссылку
+
+copy.a = 10;
+
+console.log(copy);
+console.log(obj);
+
+
+function copy(mainObj) {
+  let objCopy = {};
+
+  let key;
+  for (key in mainObj) {
+    objCopy[key] = mainObj[key];
+  }
+  return objCopy;
+}
+
+const numbers = {
+  a: 2,
+  b: 5,
+  c: {
+    x: 7,
+    y: 4
   }
 };
 
-options.makeTest();
+const newNumbers = copy(numbers);
 
-const {border, bg} = options.colors;
-console.log(border);
-// console.log(Object.keys(options));
+newNumbers.a = 10;
+newNumbers.c.x = 10;
 
-// let counter = 0;
-// for (let key in options) {
-//   if (typeof(options[key]) === 'object') {
-//     for (let i in options[key]) {
-//       console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
-//       counter++;
-//     }
-//   } else {
-//     console.log(`Свойство ${key} имеет значение ${options[key]}`);
-//     counter++;
-//   }
-// }
+console.log(newNumbers);
+console.log(numbers);
 
-// console.log(`Объект содержит ${counter} свойств`);
+const add = {
+  d: 17,
+  e: 20
+};
+
+const clone =  Object.assign({}, add);
+
+clone.d = 20;
+
+console.log(add);
+console.log(clone);
+
+const oldArray = ['a', 'b', 'c'];
+const newArray = oldArray.slice(); // создаём поверхностную копию массива
+
+newArray[1] = 'asdad';
+console.log(newArray);
+console.log(oldArray);
+
+const video = ['youtube', 'vimeo', 'rutube'],
+      blogs = ['wordpress', 'livejournal', 'blogger'],
+      internet = [...video, ...blogs, 'vk', 'telegram'];
+
+console.log(internet);
+
+function log(a, b, c) {
+  console.log(a);
+  console.log(b);
+  console.log(c);
+}
+
+const num = [2, 5, 7];
+
+log(...num);
+
+const array = ['a', 'b'];
+
+const newAarray = [...array]; // Новый способ создания поверхностной копии
+
+const q = {
+  one: 1,
+  two: 2
+};
+
+const newObj = {...q};
