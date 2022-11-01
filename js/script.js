9f88fd71c5348b83deaebcab4af8087c7917999d
Shwartz <examlpe@gmail.com>
2022-11-01 20:13:38 +0200

    рекурсия

--- js/script.js
+++ js/script.js
@@ -1,55 +1,111 @@
 /*jshint esnext: true */
-'use strict';
 
-const box = document.getElementById('box'),
-      btns = document.getElementsByTagName('button'), //получили псевдомассив
-      circles = document.getElementsByClassName('circle'),
-      wrapper = document.querySelector('.wrapper'),
-      hearts = wrapper.querySelectorAll('.heart'),
-      oneHeart = wrapper.querySelector('.heart');
-// console.dir(box);
-// box.style.backgroundColor = 'blue';
-// box.style.width = '500px';
-
-box.style.cssText = 'background-color: blue; width: 500px'; // пишем как в нормальном css
-
-btns[1].style.borderRadius = '50%';
-circles[0].style.backgroundColor = 'red';
-
-// for (let i = 0; i < hearts.length; i++) {   // на практике циклы не используют
-//   hearts[i].style.backgroundColor = 'blue';
+// function pow(x, n) {
+//   let result = 1;
+//   for (let i = 0; i < n; i++) {
+//     result *= x;
+//   }
+//   return result;
 // }
 
-hearts.forEach(item => {
-  item.style.backgroundColor = 'blue';
-});
+// function pow(x, n) {
+//   if (n === 1) {
+//     return x;
+//   } else {
+//     return x * pow(x, n - 1);
+//   }
+// }
 
-const div = document.createElement('div'); // данный элемент существует только внутри джаваскрипта
-// const text = document.createTextNode('Тут могла быть ваша реклама'); // так почти не делают
-
-div.classList.add('black');
-
-// document.body.append(div); // добавляем элемент в конец body
-// document.querySelector('.wrapper').append(div);
-wrapper.append(div);
-// wrapper.prepend(div); // вставляем элемент в начале wrapper
-
-// hearts[0].before(div);
-// hearts[1].after(div);
-
-// circles[0].remove();
-
-hearts[0].replaceWith(circles[0]);
-
-// устаревшие конструкции
-// wrapper.appendChild(div);
-// wrapper.insertBefore(div, hearts[1]);
-// wrapper.removeChild(hearts[1]);
-// wrapper.replaceChild(circles[0], hearts[0]);
-
-div.innerHTML = '<h1>Hello world</h1>';
-// div.textContent = 'Hello';
-
-div.insertAdjacentHTML('afterend', '<h2>Hello</h2>');
+// pow(2, 1); //2
+// pow(2, 2); //4
+// pow(2, 3); //8
+// pow(2, 4); // 16
 
 
+let students = {
+  js: [{
+    name: 'John',
+    progress: 100
+  }, {
+    name: 'Ivan',
+    progress: 60
+  }],
+
+  html: {
+    basic: [{
+      name: 'Peter',
+      progress: 20
+    }, {
+      name: 'Ann',
+      progress: 18
+    }],
+
+    pro: [{
+      name: 'Sam',
+      progress: 10
+    }],
+
+    semi: {
+      students: [{
+        name: 'Test',
+        progress: 100
+      }]
+    }
+  }
+};
+
+function getTotalProgressByIteration(data) {
+  let total = 0,
+      students = 0;
+  
+  for (let course of Object.values(data)) {
+    if (Array.isArray(course)) {
+      students += course.length;
+
+      for (let i = 0; i < course.length; i++) {
+        total += course[i].progress;
+      }
+    } else {
+      for (let subCourse of Object.values(course)) {
+        students += subCourse.length;
+
+        for (let i = 0; i < subCourse.length; i++) {
+          total += subCourse[i].progress;
+        }
+      }
+    }
+  }
+  return total / students;
+}
+
+// console.log(getTotalProgressByIteration(students));
+
+
+//вариант с рекурсией
+// база - массив. внутри массива могут быть только объекты со студентами
+function getTotalProgressByRecursion(data) {
+
+  if (Array.isArray(data)) {
+    let total = 0;
+
+    for (let i = 0; i < data.length; i++) {
+      total += data[i].progress;
+    }
+
+    return [total, data.length];
+  } else {
+    let total = [0, 0];
+
+    for(let subData of Object.values(data)) {
+      const subDataArr = getTotalProgressByRecursion(subData);
+      total[0] += subDataArr[0];
+      total[1] += subDataArr[1];
+    }
+
+    return total;
+  }
+}
+
+const result = getTotalProgressByRecursion(students);
+
+console.log((result[0] / result[1]).toFixed(2));
